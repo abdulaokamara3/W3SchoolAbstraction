545a63fef2db6eb7718316dc589ac1f450bce385
andrew ojuku kamara <andrewojuku7@gmail.com>
2021-04-21 03:59:42 -0700

    Initial commit

---
+++ src/com/example/Sample/ProjectAndrewJar/Dog.java
@@ -0,0 +1,4 @@
+package com.example.Sample.ProjectAndrewJar;
+
+public class Dog {
+}
---
+++ src/com/example/Sample/ProjectAndrewJar/Main.java
@@ -0,0 +1,11 @@
+package com.example.Sample.ProjectAndrewJar;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Pig myPig = new Pig(); // Create a Pig object
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
